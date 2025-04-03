c5fbd7fe2a045b30b444605b31cd6ab524623c48
Baye Dieng <bayedieng98@gmail.com>
2025-04-03 23:03:12 +0000

    parse multi file

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct SourceCode {
 }
 
 fn main() {
-    let mut llm_file = File::open("test_out1.txt").unwrap();
+    let mut llm_file = File::open("test_out2.txt").unwrap();
     let mut src_code_str = String::new();
     llm_file.read_to_string(&mut src_code_str).unwrap();
     let mut cur_idx = 0;
@@ -23,14 +23,47 @@ fn main() {
             break;
         }
         // parse the language
-        cur_idx += 3;
+        cur_idx += 4;
+        if src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
+            while !src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
+                cur_idx += 1;
+            }
+        } else {
+            cur_idx -= 1;
+        }
         let lang_first_letter_idx = cur_idx;
+        if src_code_str
+            .chars()
+            .nth(lang_first_letter_idx)
+            .unwrap()
+            .is_whitespace()
+        {
+            continue;
+        }
+
         while !src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
             cur_idx += 1;
         }
         let lang_str = &src_code_str[lang_first_letter_idx..cur_idx];
+
         while src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
             cur_idx += 1;
+        }
+        if src_code_str.chars().nth(cur_idx).unwrap() == '/' {
+            while src_code_str.chars().nth(cur_idx).unwrap() == '/' {
+                cur_idx += 1;
+            }
+
+            while src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
+                cur_idx += 1;
+            }
+
+            let file_path_first_idx = cur_idx;
+            while !src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
+                cur_idx += 1;
+            }
+            let file_path = &src_code_str[file_path_first_idx..cur_idx];
+            println!("{file_path}")
         }
         let src_code_first_idx = cur_idx;
         while src_code_str.chars().nth(cur_idx).unwrap() != '`' {
@@ -40,8 +73,8 @@ fn main() {
         // skip the backticks
         cur_idx += 3;
 
-        while !src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
-            cur_idx += 1;
-        }
+        // while !src_code_str.chars().nth(cur_idx).unwrap().is_whitespace() {
+        //     cur_idx += 1;
+        // }
     }
 }
